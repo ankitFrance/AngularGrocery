2361da504284a214fc0cafa8b9628245a5acf395
ankitFrance <ankitFrance2020@gmail.com>
2024-07-31 04:17:48 +0200

    working fine

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -19,14 +19,51 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartServiceService , private foodService: FoodService) {}
   ngOnInit(){
 
+      
+    this.loadCart();
+      
+  }
 
-      this.cartWithIds = this.cartService.getCart();
-      console.log('in cart route', this.cartWithIds)
-   
-      const allFoods = this.foodService.getAll();
-      this.cartWithDetails = this.cartWithIds.map(id => allFoods.find(food => food.id === id)!)
-      console.log('Cart Contents with Details:', this.cartWithDetails);
-   
+  loadCart() {
+    this.cartWithIds = this.cartService.getCart();
+    console.log('in cart route', this.cartWithIds);
+
+    const allFoods = this.foodService.getAll();
+    this.cartWithDetails = this.cartWithIds.map(id => allFoods.find(food => food.id === id)!);
+    console.log('Cart Contents with Details:', this.cartWithDetails);
+  }
+
+  againloadCart(){
+    const allFoods = this.foodService.getAll();
+    this.cartWithDetails = this.cartWithIds.map(id => allFoods.find(food => food.id === id)!);
+    console.log('Cart Contents with Details:', this.cartWithDetails);
+  }
+
+  removeFromCart(itemId: number): void {
+    // Retrieve the current cart from localStorage
+    const cart = localStorage.getItem('cart');
+    
+    
+    if (cart) {
+      // Parse the cart from localStorage
+      let cartArray: number[] = JSON.parse(cart);
+      
+
+      // Remove the item with the specified itemId
+      cartArray = cartArray.filter(id => id !== itemId);
+      console.log(cartArray)
+
+       // Update localStorage with the new cart
+      localStorage.setItem('cart', JSON.stringify(cartArray));
+
+      this.cartWithIds =  cartArray;
+
+      this.againloadCart()
+
+     
+
+      
+    }
   }
 
 }
